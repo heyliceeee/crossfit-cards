39268f8d41b2a76c7801d261430bb64c82394372
heyliceeee <alicedias2002@hotmail.com>
2021-11-15 09:30:38 +0000

    15.11.2021

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GlobalTrainingComponent } from './global-training/global-training.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path: 'homepage', component: HomepageComponent},
   {path: 'login', component: SigninComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'homepage/singleplayer', component: TrainingHomepageComponent}
+  {path: 'homepage/singleplayer', component: TrainingHomepageComponent},
+  {path: 'homepage/singleplayer/global', component: GlobalTrainingComponent}
 ];
 
 @NgModule({
