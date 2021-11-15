654c09618cdcf56cbbe1bc0ad6155d9fd680829d
heyliceeee <alicedias2002@hotmail.com>
2021-11-15 11:53:51 +0000

    15.11.2021

--- src/app/global-training/global-training.component.ts
+++ src/app/global-training/global-training.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-global-training',
   templateUrl: './global-training.component.html',
@@ -11,13 +13,11 @@ export class GlobalTrainingComponent implements OnInit {
 
   ngOnInit(): void {
 
-    $( document ).ready(function() {
-      var path = 'https://cdn3.iconfinder.com/data/icons/card-games-colored/48/';
-    var images = ['Games_CardGames_Artboard_64-512.png'];
+    var path = 'https://cdn3.iconfinder.com/data/icons/card-games-colored/48/';
+    var images = ['Games_CardGames_Artboard_64-512.png', 'Games_CardGames_Artboard_79-512.png'];
     var i = Math.floor(Math.random()*images.length);
 
     $('.global-cards').append("<img src='"+path+images[i]+"'>").hide().fadeIn(2000);
-    });
     
   }
 
