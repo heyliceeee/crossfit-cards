e3c069b7e125e9d3c2f300605612ea0b2e29fa42
heyliceeee <alicedias2002@hotmail.com>
2021-11-15 11:32:12 +0000

    15.11.2021

--- src/app/global-training/global-training.component.ts
+++ src/app/global-training/global-training.component.ts
@@ -10,6 +10,15 @@ export class GlobalTrainingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    $( document ).ready(function() {
+      var path = 'https://cdn3.iconfinder.com/data/icons/card-games-colored/48/';
+    var images = ['Games_CardGames_Artboard_64-512.png'];
+    var i = Math.floor(Math.random()*images.length);
+
+    $('.global-cards').append("<img src='"+path+images[i]+"'>").hide().fadeIn(2000);
+    });
+    
   }
 
 }
