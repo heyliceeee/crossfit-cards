ab3f5c768cf6053d4e31f985246a2b37ce03092e
heyliceeee <alicedias2002@hotmail.com>
2021-11-17 13:09:37 +0000

    17.11.2021

--- src/app/global-training/mocks.ts
+++ src/app/global-training/mocks.ts
@@ -3,47 +3,298 @@ import { CardTip } from "./cards";
 export const CARDTIPS: CardTip[] = [{
     "path": "../assets/cards/",
 
-    "images": ["2clubs.png", "2diamond.png", "2heart.png", "2spades.png", 
-        "3clubs.png", "3diamond.png", "3heart.png", "3spades.png",
-        "4clubs.png", "4diamond.png", "4heart.png", "4spades.png",
-        "5clubs.png", "5diamond.png", "5heart.png", "5spades.png", 
-        "6clubs.png", "6diamond.png", "6heart.png", "6spades.png", 
-        "7clubs.png", "7diamond.png", "7heart.png", "7spades.png", 
-        "8clubs.png", "8diamond.png", "8heart.png", "8spades.png", 
-        "9clubs.png", "9diamond.png", "9heart.png", "9spades.png", 
-        "10clubs.png", "10diamond.png", "10heart.png", "10spades.png", 
-        "aclubs.png", "adiamond.png", "aheart.png", "aspades.png",
-        "jackclub.png", "jackdiamond.png", "jackheart.png", "jackspades.png", 
-        "kingclub.png", "kingdiamond.png", "kingheart.png", "kingspades.png", 
-        "queenclubs.png", "queendiamond.png", "queenheart.png", "queenspades.png",
-    "joker.png"],
+    "card": [
+      {
+      "image": "2clubs.png",
+      "tips": "squats 2 reps",
+      "score": 4
+    },
+    {
+      "image": "2diamond.png",
+      "tips": "jumping jacks 2 reps",
+      "score": 4
+    },
+    {
+      "image": "2heart.png",
+      "tips": "sit-ups 2 reps",
+      "score": 4
+    },
+    {
+      "image": "2spades.png",
+      "tips": "push up 2 reps",
+      "score": 4
+    },
 
-  "tips": ["squats 2 reps", "jumping jacks 2 reps", "sit-ups 2 reps", "push up 2 reps",
-    "squats 3 reps", "jumping jacks 3 reps", "sit-ups 3 reps", "push up 3 reps",
-    "squats 4 reps", "jumping jacks 4 reps", "sit-ups 4 reps", "push up 4 reps",
-    "squats 5 reps", "jumping jacks 5 reps", "sit-ups 5 reps", "push up 5 reps",
-    "squats 6 reps", "jumping jacks 6 reps", "sit-ups 6 reps", "push up 6 reps",
-    "squats 7 reps", "jumping jacks 7 reps", "sit-ups 7 reps", "push up 7 reps",
-    "squats 8 reps", "jumping jacks 8 reps", "sit-ups 8 reps", "push up 8 reps",
-    "squats 9 reps", "jumping jacks 9 reps", "sit-ups 9 reps", "push up 9 reps",
-    "squats 10 reps", "jumping jacks 10 reps", "sit-ups 10 reps", "push up 10 reps",
-    "squats 20 reps", "jumping jacks 20 reps", "sit-ups 20 reps", "push up 20 reps",
-    "squats 11 reps", "jumping jacks 11 reps", "sit-ups 11 reps", "push up 11 reps",
-    "squats 13 reps", "jumping jacks 13 reps", "sit-ups 13 reps", "push up 13 reps",
-    "squats 12 reps", "jumping jacks 12 reps", "sit-ups 12 reps", "push up 12 reps",
-    "last exercise 30 reps"],
 
-  "score": [4, 4, 4, 4,
-    6, 6, 6, 6,
-    8, 8, 8, 8,
-    10, 10, 10, 10,
-    12, 12, 12, 12,
-    14, 14, 14, 14,
-    16, 16, 16, 16,
-    18, 18, 18, 18,
-    20, 20, 20, 20,
-    22, 22, 22, 22,
-    26, 26, 26, 26,
-    24, 24, 24, 24,
-    60]
-}];
+    {
+      "image": "3clubs.png",
+      "tips": "squats 3 reps",
+      "score": 6
+    },
+    {
+      "image": "3diamond.png",
+      "tips": "jumping jacks 3 reps",
+      "score": 6
+    },
+    {
+      "image": "3heart.png",
+      "tips": "sit-ups 3 reps",
+      "score": 6
+    },
+    {
+      "image": "3spades.png",
+      "tips": "push up 3 reps",
+      "score": 6
+    },
+
+
+    {
+      "image": "4clubs.png",
+      "tips": "squats 4 reps",
+      "score": 8
+    },
+    {
+      "image": "4diamond.png",
+      "tips": "jumping jacks 4 reps",
+      "score": 8
+    },
+    {
+      "image": "4heart.png",
+      "tips": "sit-ups 4 reps",
+      "score": 8
+    },
+    {
+      "image": "4spades.png",
+      "tips": "push up 4 reps",
+      "score": 8
+    },
+
+
+    {
+      "image": "5clubs.png",
+      "tips": "squats 5 reps",
+      "score": 10
+    },
+    {
+      "image": "5diamond.png",
+      "tips": "jumping jacks 5 reps",
+      "score": 10
+    },
+    {
+      "image": "5heart.png",
+      "tips": "sit-ups 5 reps",
+      "score": 10
+    },
+    {
+      "image": "5spades.png",
+      "tips": "push up 5 reps",
+      "score": 10
+    },
+
+
+    {
+      "image": "6clubs.png",
+      "tips": "squats 6 reps",
+      "score": 12
+    },
+    {
+      "image": "6diamond.png",
+      "tips": "jumping jacks 6 reps",
+      "score": 12
+    },
+    {
+      "image": "6heart.png",
+      "tips": "sit-ups 6 reps",
+      "score": 12
+    },
+    {
+      "image": "6spades.png",
+      "tips": "push up 6 reps",
+      "score": 12
+    },
+
+
+    {
+      "image": "7clubs.png",
+      "tips": "squats 7 reps",
+      "score": 14
+    },
+    {
+      "image": "7diamond.png",
+      "tips": "jumping jacks 7 reps",
+      "score": 14
+    },
+    {
+      "image": "7heart.png",
+      "tips": "sit-ups 7 reps",
+      "score": 14
+    },
+    {
+      "image": "7spades.png",
+      "tips": "push up 7 reps",
+      "score": 14
+    },
+
+
+    {
+      "image": "8clubs.png",
+      "tips": "squats 8 reps",
+      "score": 16
+    },
+    {
+      "image": "8diamond.png",
+      "tips": "jumping jacks 8 reps",
+      "score": 16
+    },
+    {
+      "image": "8heart.png",
+      "tips": "sit-ups 8 reps",
+      "score": 16
+    },
+    {
+      "image": "8spades.png",
+      "tips": "push up 8 reps",
+      "score": 16
+    },
+
+
+    {
+      "image": "9clubs.png",
+      "tips": "squats 9 reps",
+      "score": 18
+    },
+    {
+      "image": "9diamond.png",
+      "tips": "jumping jacks 9 reps",
+      "score": 18
+    },
+    {
+      "image": "9heart.png",
+      "tips": "sit-ups 9 reps",
+      "score": 18
+    },
+    {
+      "image": "9spades.png",
+      "tips": "push up 9 reps",
+      "score": 18
+    },
+
+
+    {
+      "image": "10clubs.png",
+      "tips": "squats 10 reps",
+      "score": 20
+    },
+    {
+      "image": "10diamond.png",
+      "tips": "jumping jacks 10 reps",
+      "score": 20
+    },
+    {
+      "image": "10heart.png",
+      "tips": "sit-ups 10 reps",
+      "score": 20
+    },
+    {
+      "image": "10spades.png",
+      "tips": "push up 10 reps",
+      "score": 20
+    },
+
+
+    {
+      "image": "aclubs.png",
+      "tips": "squats 20 reps",
+      "score": 40
+    },
+    {
+      "image": "adiamond.png",
+      "tips": "jumping jacks 20 reps",
+      "score": 40
+    },
+    {
+      "image": "aheart.png",
+      "tips": "sit-ups 20 reps",
+      "score": 40
+    },
+    {
+      "image": "aspades.png",
+      "tips": "push up 20 reps",
+      "score": 40
+    },    
+
+
+    {
+      "image": "jackclubs.png",
+      "tips": "squats 11 reps",
+      "score": 22
+    },
+    {
+      "image": "jackdiamond.png",
+      "tips": "jumping jacks 11 reps",
+      "score": 22
+    },
+    {
+      "image": "jackheart.png",
+      "tips": "sit-ups 11 reps",
+      "score": 22
+    },
+    {
+      "image": "jackspades.png",
+      "tips": "push up 11 reps",
+      "score": 22
+    },
+
+
+    {
+      "image": "kingclubs.png",
+      "tips": "squats 13 reps",
+      "score": 26
+    },
+    {
+      "image": "kingdiamond.png",
+      "tips": "jumping jacks 13 reps",
+      "score": 26
+    },
+    {
+      "image": "kingheart.png",
+      "tips": "sit-ups 13 reps",
+      "score": 26
+    },
+    {
+      "image": "kingspades.png",
+      "tips": "push up 13 reps",
+      "score": 26
+    },
+
+
+    {
+      "image": "queenclubs.png",
+      "tips": "squats 12 reps",
+      "score": 24
+    },
+    {
+      "image": "queendiamond.png",
+      "tips": "jumping jacks 12 reps",
+      "score": 24
+    },
+    {
+      "image": "queenheart.png",
+      "tips": "sit-ups 12 reps",
+      "score": 24
+    },
+    {
+      "image": "queenspades.png",
+      "tips": "push up 12 reps",
+      "score": 24
+    },
+
+
+    {
+      "image": "joker.png",
+      "tips": "last exercise 30 reps",
+      "score": 60
+    }
+  ]
+}
+];
